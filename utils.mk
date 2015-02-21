74609f90704c25f0bc5a9d608fcc22e3e685e9f4
Michal Wirth <wirthmi@rankl.cz>
2015-02-21 14:37:54 +0100

    generalize concept of dependency files to be usable not only in build

--- utils.mk
+++ utils.mk
@@ -10,3 +10,21 @@ FUNCTION_FIND_FILES = $(shell \
 
 # usage: $(call FUNCTION_DROP_REDUNDANT_SLASHES,path)
 FUNCTION_DROP_REDUNDANT_SLASHES = $(shell echo $(1) |sed 's/\/\/*/\//g')
+
+
+# === CONCEPT OF DEPENDENCY FILES =========
+
+# when a dependency file already exists while compiling the object, it should
+# be used to decide whether it is necessary to recompile the object or not,
+# when dependency file doesn't exist the recompilation is forced because it may
+# be necessary to do it
+
+# usage: $(call FUNCTION_SOLVE_DEPENDENCIES,dependency_file)
+FUNCTION_SOLVE_DEPENDENCIES = $(shell \
+	if [ -r $(1) ]; then cat $(1); else echo FORCE; fi \
+)
+
+# usage: $(call FUNCTION_GET_DEPENDENCY_FILE_FORMATTER,source_suffix)
+FUNCTION_GET_DEPENDENCY_FILE_FORMATTER = \
+	sed -n '1h;1!H;$${g;s/^[^:]*://;s/[ \\]/\n/g;p}' | \
+	sed '/^$$/d;/\.$(1)$$/d'
