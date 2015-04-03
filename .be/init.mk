75dedaf1ac993b0399429f6ccc69438c824342d7
Michal Wirth <wirthmi@rankl.cz>
2015-04-03 23:29:27 +0200

    fix missing sort for loading of includes in init

--- .be/init.mk
+++ .be/init.mk
@@ -13,13 +13,13 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    
+
 # see http://www.gnu.org/software/make/manual/make.html for Makefile syntax
 
 PATH_TO_BE_DIRECTORY = $(dir $(filter %init.mk,$(MAKEFILE_LIST)))
 
 # load all internals of build enviroment in strictly defined order
-include $(wildcard $(PATH_TO_BE_DIRECTORY)/??-*.mk)
+include $(sort $(wildcard $(PATH_TO_BE_DIRECTORY)/??-*.mk))
 
 # load some basic shared targets, nothing fancy
 include $(PATH_TO_TARGET_DIRECTORY)/global.mk
