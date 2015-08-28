4838a123e4197d9ead27a7d2d6d3bee374013335
Michal Wirth <wirthmi@rankl.cz>
2015-08-28 19:55:33 +0200

    add example of file specific CXXFLAGS

--- src/project.cpp
+++ src/project.cpp
@@ -1,10 +1,14 @@
 #include <cstdlib>
 #include <iostream>
 
+// CXXFLAGS = -DWAY_TO_SET_FILE_SPECIFIC_CXXFLAGS
+
 
 int main( int argc, char * argv[] ) {
 
+  #ifdef WAY_TO_SET_FILE_SPECIFIC_CXXFLAGS
   std::cout << "Hello, World!" << std::endl;
+  #endif
 
   return EXIT_SUCCESS;
 }
